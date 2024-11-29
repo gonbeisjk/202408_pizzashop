efa7c408571c11412e9465b1253d6ed52b5f8097
gonbeisjk <gonbeisjk@gmail.com>
2024-11-29 12:42:21 +0900

    データベースのバックアップを更新

--- backup.sql
+++ backup.sql
@@ -1,54 +1,62 @@
--- MySQL dump 10.13  Distrib 8.0.35, for macos10.15 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: pizzashop
--- ------------------------------------------------------
--- Server version	8.0.35
+-- ホスト: localhost:3306
+-- 生成日時: 2024-11-29 03:41:27
+-- サーバのバージョン： 5.7.24
+-- PHP のバージョン: 8.3.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 --
--- Table structure for table `pizzas`
+-- データベース: `pizzashop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `pizzas`
 --
 
 DROP TABLE IF EXISTS `pizzas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pizzas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `pizza_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `chef_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `toppings` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `pizza_name` varchar(255) NOT NULL,
+  `chef_name` varchar(255) NOT NULL,
+  `toppings` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pizzas`
+-- テーブルのデータのダンプ `pizzas`
 --
 
-LOCK TABLES `pizzas` WRITE;
-/*!40000 ALTER TABLE `pizzas` DISABLE KEYS */;
-INSERT INTO `pizzas` VALUES (1,'IPスペシャル','ピザ太郎','トマト,バジル,モッツァレラチーズ','2024-11-28 02:12:11'),(2,'クァトロフォルマッジ','ピザンヌ','水牛モッツァレラ,ゴルゴンゾーラ,タレッジョ,パルミジャーノ','2024-11-28 02:21:32'),(5,'タランティーナ','よしだ','ピーマン,ベーコン','2024-11-28 05:59:21');
-/*!40000 ALTER TABLE `pizzas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `pizzas` (`id`, `pizza_name`, `chef_name`, `toppings`, `created_at`) VALUES
+(1, 'IPスペシャル', 'ピザ太郎', 'トマト,バジル,モッツァレラチーズ', '2024-11-27 17:12:11'),
+(2, 'クァトロフォルマッジ', 'ピザンヌ', '水牛モッツァレラ,ゴルゴンゾーラ,タレッジョ,パルミジャーノ', '2024-11-27 17:21:32'),
+(5, 'タランティーナ', 'よしだ', 'ピーマン,ベーコン', '2024-11-27 20:59:21'),
+(6, 'あいうえお', 'かきくけこ', '', '2024-11-29 01:40:50');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- ダンプしたテーブルのインデックス
+--
 
--- Dump completed on 2024-11-28 15:33:04
+--
+-- テーブルのインデックス `pizzas`
+--
+ALTER TABLE `pizzas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `pizzas`
+--
+ALTER TABLE `pizzas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
